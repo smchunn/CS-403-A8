7e03bf7d0638ae6f9ddde90d3dc7271ff0346475
Spencer Chunn <smcnola1@gmail.com>
2021-11-20 10:49:17 -0600

    added list function

--- functions.pl
+++ functions.pl
@@ -1,3 +1,11 @@
+list(Op, Ref, List) :- (
+	Op='eq', findall([X, Y, Z], (pay(X, Y, Z), Z=:=Ref), List);
+	Op='ne', findall([X, Y, Z], (pay(X, Y, Z), Z=\=Ref), List);
+	Op='gt', findall([X, Y, Z], (pay(X, Y, Z), Z>Ref), List);
+	Op='ge', findall([X, Y, Z], (pay(X, Y, Z), Z>=Ref), List);
+	Op='lt', findall([X, Y, Z], (pay(X, Y, Z), Z<Ref), List);
+	Op='le', findall([X, Y, Z], (pay(X, Y, Z), Z=<Ref), List)
+	).
 count(Op, Ref, Count) :- (
 	Op='eq', findall(Z, (pay(X, Y, Z), Z=:=Ref), List), length(List, Count);
 	Op='ne', findall(Z, (pay(X, Y, Z), Z=\=Ref), List), length(List, Count);
