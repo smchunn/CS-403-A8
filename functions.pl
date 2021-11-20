49d97e860d95ff8d0a412872d68cae72536fe4cb
Spencer Chunn <smcnola1@gmail.com>
2021-11-20 10:32:45 -0600

    count function added

---
+++ functions.pl
@@ -0,0 +1,8 @@
+count(Op, Ref, Count) :- (
+	Op='eq', findall(Z, (pay(X, Y, Z), Z=:=Ref), List), length(List, Count);
+	Op='ne', findall(Z, (pay(X, Y, Z), Z=/=Ref), List), length(List, Count);
+	Op='gt', findall(Z, (pay(X, Y, Z), Z>Ref), List), length(List, Count);
+	Op='ge', findall(Z, (pay(X, Y, Z), Z>=Ref), List), length(List, Count);
+	Op='lt', findall(Z, (pay(X, Y, Z), Z<Ref), List), length(List, Count);
+	Op='le', findall(Z, (pay(X, Y, Z), Z=<Ref), List), length(List, Count)
+).
